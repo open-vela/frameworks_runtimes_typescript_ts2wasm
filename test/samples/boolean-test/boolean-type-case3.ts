67a0d4e899bfabae2dd77cec978fba651529b344
zhenweijin <109658203+zhenweijin@users.noreply.github.com>
2022-10-19 17:36:40 +0800

    update: google test and git action for dyntype (#104)
    * merge main
    
    * update: dyntype testing
    
    * test git action
    
    * update: separate git action files, format fix
    
    * fix: string eq and externref test
    
    * fix: separate sample test files
    
    * update: dump values

---
+++ test/samples/boolean-test/boolean-type-case3.ts
@@ -0,0 +1,5 @@
+export function booleanTestCase3(): boolean {
+    const j1 = 123;
+    const i4 = !j1;
+    return i4;
+}
---
+++ test/samples/boolean-test/boolean-type-case6.ts
@@ -0,0 +1,6 @@
+export function booleanTestCase6(): boolean {
+    const j7 = 123,
+        j8 = 456;
+    const i8 = j7 > 200 || j8 < 200;
+    return i8;
+}
