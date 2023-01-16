93365d1a63c6995fd802c126000d58fd3156e0f8
zhenweijin <109658203+zhenweijin@users.noreply.github.com>
2023-01-16 09:46:15 +0800

    bug fix (#206)
    * add class type
    
    * fix bugs

--- src/variable.ts
+++ src/variable.ts
@@ -12,7 +12,7 @@ import {
     getNearestFunctionScopeFromCurrent,
     generateNodeExpression,
 } from './utils.js';
-import { FunctionScope, GlobalScope, Scope } from './scope.js';
+import { FunctionScope, GlobalScope, Scope, ScopeKind } from './scope.js';
 
 export enum ModifierKind {
     default,
@@ -187,23 +187,7 @@ export class VariableScanner {
             case ts.SyntaxKind.VariableDeclaration: {
                 const variableDeclarationNode = <ts.VariableDeclaration>node;
                 const currentScope = this.getCurrentScope();
-                const functionScope =
-                    getNearestFunctionScopeFromCurrent(currentScope);
-                let isLocalVar = true;
-                let nearestScope;
-                let variableIndex;
-                if (!functionScope) {
-                    isLocalVar = false;
-                    nearestScope = <GlobalScope>(
-                        currentScope.getRootGloablScope()
-                    );
-                    variableIndex = nearestScope.varArray.length;
-                } else {
-                    nearestScope = <FunctionScope>functionScope;
-                    variableIndex =
-                        nearestScope.paramArray.length +
-                        nearestScope.varArray.length;
-                }
+
                 let variableModifier = ModifierKind.default;
                 const variableAssignText =
                     variableDeclarationNode.parent.getText();
@@ -229,14 +213,49 @@ export class VariableScanner {
                                   this.typechecker!,
                               ).typeName,
                           );
-                const variableObj = new Variable(
+                let isLocalVar = true;
+                const variable = new Variable(
                     variableName,
                     variableType,
                     variableModifier,
-                    variableIndex,
+                    -1,
                     isLocalVar,
                 );
-                currentScope.addVariable(variableObj);
+                /* iff in a global scope, set index based on global scope variable array */
+                if (currentScope.kind === ScopeKind.GlobalScope) {
+                    isLocalVar = false;
+                    variable.setVarIndex(currentScope.varArray.length);
+                }
+                const functionScope =
+                    getNearestFunctionScopeFromCurrent(currentScope);
+                /* under global scope, set index based on start function variable array */
+                if (!functionScope) {
+                    if (currentScope.getRootGloablScope() === null) {
+                        throw new Error('global scope is null');
+                    }
+                    variable.setVarIndex(
+                        (<GlobalScope>currentScope.getRootGloablScope())
+                            .startFuncVarArray.length,
+                    );
+                    (<GlobalScope>(
+                        currentScope.getRootGloablScope()
+                    )).addStartFuncVar(variable);
+                } else {
+                    /* under function scope, set index based on function variable array */
+                    variable.setVarIndex(functionScope.varArray.length);
+                    if (currentScope.kind !== ScopeKind.FunctionScope) {
+                        functionScope.addVariable(variable);
+                    }
+                }
+                if (variable.varIndex === -1) {
+                    throw new Error(
+                        'variable index is not set, variable name is <' +
+                            variable.varName +
+                            '>',
+                    );
+                }
+                currentScope.addVariable(variable);
+
                 break;
             }
         }
