878d363ecece77ff862574be1ab8fbd4549b61d3
zhenweijin <109658203+zhenweijin@users.noreply.github.com>
2023-03-03 10:41:44 +0800

    Binaryen ver112 (#333)
    * update binaryen version from 110 to 112
    
    * update wasm-shell & iwasm_gc for docker
    
    * add binaryen interpreter

---
+++ tools/gcInterpreter/scripts/wastGen.sh
@@ -0,0 +1,13 @@
+scriptDir=$(cd "$(dirname "$0")" && pwd)
+ts2wasm=$scriptDir/../../../build/cli/ts2wasm.js
+samplePath=${1:-$scriptDir/../../../tests/samples}
+samples=$(ls $samplePath)
+outputPath=$scriptDir/../output/wasmFiles
+
+# any type is disabled
+for sampleFile in $samples
+    do
+        sampleName=$(echo $sampleFile | cut -d . -f1)
+        node $ts2wasm $samplePath/$sampleFile --disableAny --output $outputPath/$sampleName.wasm --wat $outputPath/$sampleName.wat
+        rm -f $outputPath/$sampleName.wasm
+    done
