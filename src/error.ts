6e9e36195f5a1d336474fb1a0dc59a5306af1d84
yviansu <103162767+yviansu@users.noreply.github.com>
2023-03-07 17:47:38 +0800

    add log config (#335)
    * add log config
    
    * use log4js.ts instead of log4js.json
    
    * fix killinig nodejs process from generating logs
    
    * fix killinig nodejs error
    
    * adjust error structure

---
+++ src/error.ts
@@ -0,0 +1,11 @@
+export class SyntaxError extends Error {
+    constructor(message: string) {
+        super(message);
+    }
+}
+
+export class UnimplementError extends Error {
+    constructor(message: string) {
+        super(message);
+    }
+}
