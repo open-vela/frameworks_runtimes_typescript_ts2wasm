c0fdbe5465a58eaab522e8a591d7973f8efd0c3b
yviansu <103162767+yviansu@users.noreply.github.com>
2023-03-09 17:25:33 +0800

    Fix MethodAccess (#348)
    * fix methodaccess
    
    * remove unused code
    
    * add test sample
    
    * fix functionAccess call

--- tests/samples/class-case1.ts
+++ tests/samples/class-case1.ts
@@ -3,9 +3,15 @@ class A1 {
     test() {
         return 'xyz';
     }
+
+    test2() {
+        return 1;
+    }
 }
 
 export function classTest2() {
     let a: A1 = new A1();
     let b = a.test();
+    let test2Func = a.test2;
+    const num = test2Func();
 }
--- tests/samples/class-case13.ts
+++ tests/samples/class-case13.ts
@@ -1,11 +1,17 @@
 class A13 {
-    static test() {
+    static test1() {
         return 1;
+    }
+
+    static test2() {
+        return 2;
     }
     // eslint-disable-next-line @typescript-eslint/no-empty-function
     constructor() {}
 }
 
 function classTest13() {
-    A13.test();
+    const func = A13.test1;
+    func();
+    const var2 = A13.test2();
 }
