722cf849802775f55b5b756ce266cf36c0575111
Xu Jun <jun1.xu@intel.com>
2023-03-24 14:05:09 +0800

    enhance browser validator (#381)

--- ts2wasm/tools/validate/browser/index.ts
+++ ts2wasm/tools/validate/browser/index.ts
@@ -14,18 +14,18 @@ const SAMPLES_DIR = path.join(SCRIPT_DIR, '../../../tests/samples');
 const COMPILE_DIR = path.join(SCRIPT_DIR, 'wasm_modules');
 
 if (fs.existsSync(COMPILE_DIR)) {
-    fs.rmSync(COMPILE_DIR, { recursive: true })
+    fs.rmSync(COMPILE_DIR, { recursive: true });
 }
 
 fs.mkdirSync(COMPILE_DIR);
 
 let tsFiles = fs.readdirSync(SAMPLES_DIR);
 tsFiles = tsFiles.filter((f) => f !== 'sample_cases.test.ts');
-console.log('compiling source files ...')
+console.log('compiling source files ...');
 tsFiles.forEach((file) => {
     const currentTsFile = `${SAMPLES_DIR}/${file}`;
 
-    if (fs.lstatSync(currentTsFile).isDirectory())  return;
+    if (fs.lstatSync(currentTsFile).isDirectory()) return;
 
     const outputFile = `${COMPILE_DIR}/${path.parse(file).name}.wasm`;
 
@@ -39,9 +39,10 @@ tsFiles.forEach((file) => {
         backend.codegen();
         const wasmBuffer = backend.emitBinary();
         fs.writeFileSync(outputFile, wasmBuffer);
-    }
-    catch {
+        backend.dispose();
+    } catch {
         console.error(`Compiling [${file}] failed`);
     }
-})
+});
 
+process.exit(0);
