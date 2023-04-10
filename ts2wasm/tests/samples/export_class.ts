4ac1bf4e8c404706ed5e9910e08a239e17ad3528
yviansu <103162767+yviansu@users.noreply.github.com>
2023-04-10 11:25:50 +0800

    change import, export, module, declare, namespace samples (#432)

--- ts2wasm/tests/samples/export_class.ts
+++ ts2wasm/tests/samples/export_class.ts
@@ -3,6 +3,22 @@
  * SPDX-License-Identifier: Apache-2.0 WITH LLVM-exception
  */
 
+export class ExportedClass {
+    _a = 8;
+
+    set a(v: number) {
+        this._a = v;
+    }
+
+    get a(): number {
+        return this._a;
+    }
+
+    setA(a: number) {
+        this._a = a;
+    }
+}
+
 export default class DefaultExportClass {
     public static foo(x: number) {
         return x + 1;
