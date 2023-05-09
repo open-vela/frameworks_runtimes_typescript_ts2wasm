0bd2f1b34df5819cec4fba0cfc0293298d728561
zhenweijin <109658203+zhenweijin@users.noreply.github.com>
2023-05-09 12:07:48 +0800

    add custom type name (#496)
    * add custom type name
    
    * change function name
    
    * change function name

--- ts2wasm/src/backend/binaryen/index.ts
+++ ts2wasm/src/backend/binaryen/index.ts
@@ -240,6 +240,7 @@ export class WASMGen extends Ts2wasmBackend {
     private startBodyArray: Array<binaryen.ExpressionRef> = [];
     private globalInitArray: Array<binaryen.ExpressionRef> = [];
     private globalInitFuncName = '';
+    private wasmStringMap = new Map<string, number>();
 
     constructor(parserContext: ParserContext) {
         super(parserContext);
@@ -1060,5 +1061,20 @@ export class WASMGen extends Ts2wasmBackend {
             wrapperName: wrappedName,
             wrapperType: wrapperFuncType,
         };
+    }
+
+    public getCString(str: string) {
+        if (this.wasmStringMap.has(str)) {
+            return this.wasmStringMap.get(str) as number;
+        }
+        const wasmStr = binaryenCAPI._malloc(str.length + 1);
+        let index = wasmStr;
+        // consider UTF-8 only
+        for (let i = 0; i < str.length; i++) {
+            binaryenCAPI.__i32_store8(index++, str.codePointAt(i) as number);
+        }
+        binaryenCAPI.__i32_store8(index, 0);
+        this.wasmStringMap.set(str, wasmStr);
+        return wasmStr;
     }
 }
