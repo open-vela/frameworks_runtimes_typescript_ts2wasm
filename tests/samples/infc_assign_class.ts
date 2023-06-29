ed8933ca0bcd94ec7e3c0f82ee4a76663a85728e
zhenweijin <109658203+zhenweijin@users.noreply.github.com>
2023-06-29 11:54:14 +0800

    add impl id for object for optimization (#674)
    * add impl in the frontend
    
    * implement interface impl in the backend
    
    * add description for infc fields

--- tests/samples/infc_assign_class.ts
+++ tests/samples/infc_assign_class.ts
@@ -22,3 +22,41 @@ export function classAndInfc() {
     const f: Foo = i;
     return f.x;
 }
+
+
+interface I2 {
+    y: string;
+    x: number;
+    z: () => number;
+    set m(v: number);
+    get m();
+}
+
+class A implements I2 {
+    x: number;
+    y: string;
+    _m: number;
+    get m() {
+        return this._m;
+    }
+    set m(v: number) {
+        this._m = v;
+    }
+    constructor(xx: number, yy: string, mm: number) {
+        this.x = xx;
+        this.y = yy;
+        this._m = mm;
+    }
+    z() {
+        return this.m + this.x;
+    }
+}
+
+export function infcImpl() {
+    const i: I2 = new A(1, '2', 2);
+    if (i.y === '2') {
+        i.m = 10;
+        return i.z() + i.m;
+    }
+    return 0;
+}
