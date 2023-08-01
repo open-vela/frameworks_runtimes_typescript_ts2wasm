83a65201cc183088878912a98b10a6f63c2471e9
Xu Jun <jun1.xu@intel.com>
2023-08-02 02:26:11 +0800

    pass env parameters to native APIs (#805)
    Change-Id: I6f0456d2fa6084c4be2e0a6c0f6de275e5c719f4

--- runtime-library/stdlib/lib_console.c
+++ runtime-library/stdlib/lib_console.c
@@ -9,12 +9,14 @@
 #include "dyntype.h"
 
 void *
-console_constructor(wasm_exec_env_t exec_env, void *obj)
+console_constructor(wasm_exec_env_t exec_env, void *context, void *thiz,
+                    void *obj)
 {
     return obj;
 }
 
-void console_log(wasm_exec_env_t exec_env, void *obj)
+void
+console_log(wasm_exec_env_t exec_env, void *context, void *thiz, void *obj)
 {
     uint32 i, len;
     wasm_value_t wasm_array_data = { 0 }, wasm_array_len = { 0 };
@@ -50,8 +52,8 @@ void console_log(wasm_exec_env_t exec_env, void *obj)
     { #func_name, func_name, signature, NULL }
 
 static NativeSymbol native_symbols[] = {
-    REG_NATIVE_FUNC(console_constructor, "()r"),
-    REG_NATIVE_FUNC(console_log, "(r)"),
+    REG_NATIVE_FUNC(console_constructor, "(rr)r"),
+    REG_NATIVE_FUNC(console_log, "(rrr)"),
     /* TODO */
 };
 /* clang-format on */
