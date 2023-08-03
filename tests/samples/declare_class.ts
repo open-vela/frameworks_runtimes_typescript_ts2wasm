06dc8fb8fef52f4679fa37c7142fab13222ddf48
Su Yihan <103162767+yviansu@users.noreply.github.com>
2023-08-03 14:39:40 +0800

    fix getter/setter error in declared class (#813)
    Signed-off-by:huke<huke@xiaomi.com>

--- tests/samples/declare_class.ts
+++ tests/samples/declare_class.ts
@@ -8,9 +8,16 @@ declare class DeclaredClass {
     constructor(grade: number);
     sayHello(): void;
     static whoSayHi(name: string): number;
+    get value(): any;
+    set value(v: number);
 }
 
 export function classDecl() {
     const sayHiFunc = DeclaredClass.whoSayHi('i');
-    return sayHiFunc;
+    console.log(sayHiFunc);
+    const dc = new DeclaredClass(99);
+    console.log(dc.grade);
+    dc.value = 100;
+    console.log(dc.value);
+    dc.sayHello();
 }
