6403c3523f0264109b3864e14f07bc1ce24ab2c5
Xu Jun <jun1.xu@intel.com>
2023-08-14 21:53:53 +0800

    control gc every allocation by macro (#847)
    Signed-off-by: ganjing <ganjing@xiaomi.com>

--- runtime-library/wamr_config.cmake
+++ runtime-library/wamr_config.cmake
@@ -19,7 +19,15 @@ if (NOT DEFINED WAMR_BUILD_FAST_INTERP)
     set (WAMR_BUILD_FAST_INTERP 1)
 endif()
 
-# Force GC in every allocation during testing
-add_definitions(-DGC_IN_EVERY_ALLOCATION=1)
+if (WAMR_GC_IN_EVERY_ALLOCATION EQUAL 1)
+    message("* Garbage collection in every allocation: on")
+    # Force GC in every allocation during testing
+    add_definitions(-DGC_IN_EVERY_ALLOCATION=1)
+endif()
+
+if (DEFINED WAMR_GC_HEAP_SIZE)
+    message("* Set WAMR gc heap size: ${WAMR_GC_HEAP_SIZE}")
+    add_definitions(-DGC_HEAP_SIZE_DEFAULT=${WAMR_GC_HEAP_SIZE})
+endif()
 
 include(${WAMR_DIR}/build-scripts/runtime_lib.cmake)
