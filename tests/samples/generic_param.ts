402f146aeb8fa4e9aaf97e84fd85ef10ff1bcde3
Shanks0224 <130947810+Shanks0224@users.noreply.github.com>
2023-09-22 12:00:56 +0800

    feat: implement specialization of type aliases (#962)
    Signed-off-by: ganjing <ganjing@xiaomi.com>

--- tests/samples/generic_param.ts
+++ tests/samples/generic_param.ts
@@ -11,6 +11,11 @@ function genericFunc<T>(num: T) {
     anyFunc(num);
 }
 
+type ForCallback<T> = (item: T, index: number) => number
+
 export function testGenericParam() {
     genericFunc('hi');
+
+    const fn: ForCallback<number> = (item: number, index: number): number => {return item + index}
+    anyFunc(fn(1, 2));
 }
