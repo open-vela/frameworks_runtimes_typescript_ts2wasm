46e66ecdd318a6a18bdadd580a163b0fe91ddec0
Su Yihan <yihan.su@intel.com>
2023-09-22 14:40:27 +0800

    operate nullable objects in if condition (#958)
    Change-Id: I54b0848b9b9ab3a0217d4aeac625e2374ada7425

---
+++ tests/samples/op_ref_type.ts
@@ -0,0 +1,24 @@
+export function judgeIsRefNull() {
+    const accounts = [{a: 1}, {a: 2}];
+    const account1 = accounts[0];
+    const account2 = accounts[1];
+    const account3 = null;
+
+    if (account1 && account2) {
+        console.log('both find')
+    } else {
+        console.log('at least one found');
+    }
+
+    if (account1 && account3) {
+        console.log('both find')
+    } else {
+        console.log('at least one found');
+    }
+
+    if (account1 || account3) {
+        console.log('one find')
+    } else {
+        console.log('no found');
+    }
+}
